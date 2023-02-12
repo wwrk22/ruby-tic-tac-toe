4a8e7ab360e9b7a0ef29ddb885c307e32fbfc9b9
Won Rhim <wwrk22@gmail.com>
2023-02-11 20:28:22 -0500

    Redo game board and add tests

---
+++ board.rb
@@ -0,0 +1,103 @@
+class Board
+
+  ##
+  # Create a new board with all 9 slots empty as indicated by nil.
+  def initialize
+    @game_board = Array.new(9)
+  end
+
+  ##
+  # Clear the board by resetting all 9 slots of @game_board to nil.
+  def clear_board
+    @game_board.clear
+    puts "Cleared board."
+  end
+
+
+  ##
+  # Return the board in current state in string format.
+  def get_board
+    board_str = ""
+    
+    @game_board.each_with_index do |slot, index|
+      board_str << "["
+      if slot
+        board_str << slot
+      else
+        board_str << " "
+      end
+      board_str << "]"
+
+      if (index + 1) % 3 == 0
+        board_str << "\n"
+      end
+    end
+
+    return board_str
+  end
+
+  ##
+  # Check each row for a winner and return the marker if there is one.
+  # Return nil if no row has a winner.
+  def check_rows
+    index = 0
+
+    while index < 9 do
+      row_markers = @game_board[index..(index + 2)].uniq
+
+      if row_markers.size == 1 && row_markers[0] != nil
+        return row_markers[0]
+      end
+
+      index += 3
+    end
+
+    return nil
+  end
+
+  ##
+  # Check each column for a winner and return the marker if there is one.
+  # Return nil if no column has a winner.
+  def check_columns
+    index = 0
+
+    while index < 3 do
+      column_markers = [
+        @game_board[index],
+        @game_board[index + 3],
+        @game_board[index + 6]
+      ]
+      column_markers = column_markers.uniq
+
+      if column_markers.size == 1 && column_markers[0] != nil
+        return column_markers[0]
+      end
+      
+      index += 1
+    end
+
+    return nil
+  end
+
+  ##
+  # Check both diagonals for a winner and return the marker if there is one.
+  # Return nil if no diagonal has a winner.
+  def check_diagonals
+    diagonal_markers = [@game_board[0], @game_board[4], @game_board[8]]
+    diagonal_markers = diagonal_markers.uniq
+
+    if diagonal_markers.size == 1 && diagonal_markers[0] != nil
+      return diagonal_markers[0]
+    end
+
+    diagonal_markers = [@game_board[2], @game_board[4], @game_board[6]]
+    diagonal_markers = diagonal_markers.uniq
+
+    if diagonal_markers.size == 1 && diagonal_markers[0] != nil
+      return diagonal_markers[0]
+    end
+
+    return nil
+  end
+
+end
---
+++ spec/board_spec.rb
@@ -0,0 +1,218 @@
+# frozen_string_literal: true
+
+require_relative '../board.rb'
+
+RSpec.describe Board do
+
+  # Suppress print and puts messages
+  before :all do
+    @stdout = $stdout
+    @stderr = $stderr
+    $stdout = File.open(File::NULL, 'w')
+    $stderr = File.open(File::NULL, 'w')
+  end
+
+  describe '#clear_board' do
+    let(:game_board) { instance_double(Array) }
+    subject(:board) { described_class.new }
+
+    before do
+      allow(Array).to receive(:new).and_return(game_board)
+      allow(game_board).to receive(:clear)
+    end
+
+    it "sends #clear to @board" do
+      expect(game_board).to receive(:clear)
+      board.clear_board
+    end
+  end
+
+
+  describe '#get_board' do
+    context "when the board is empty" do
+      subject(:empty_board) { described_class.new }
+      let(:expected_board) { "[ ][ ][ ]\n[ ][ ][ ]\n[ ][ ][ ]\n" }
+
+      it "returns the empty board" do
+        expect(empty_board.get_board).to eq(expected_board)
+      end
+    end
+
+    context "when the board has markers on it" do
+      subject(:marked_board) { described_class.new }
+      let(:expected_board) { "[X][O][ ]\n[ ][ ][ ]\n[ ][ ][ ]\n" }
+      let(:actual_board) {
+        board = Array.new(9)
+        board[0] = 'X'
+        board[1] = 'O'
+        board
+      }
+      
+      before do
+        allow(Array).to receive(:new).and_return(actual_board)
+      end
+
+      it "returns the correct board" do
+        expect(marked_board.get_board).to eq(expected_board)
+      end
+    end
+
+    context "when the board is full" do
+      subject(:full_board) { described_class.new }
+      let(:expected_board) { "[O][O][O]\n[O][O][O]\n[O][O][O]\n" }
+      let(:actual_board) { %w(O O O O O O O O O) }
+
+      before do
+        allow(Array).to receive(:new).and_return(actual_board)
+      end
+
+      it "returns the full board" do
+        expect(full_board.get_board).to eq(expected_board)
+      end
+    end
+  end
+
+
+  describe '#check_rows' do
+    context "when no rows indicate a winner" do
+      context "when no rows have any markers" do
+        subject(:empty_board) { described_class.new }
+
+        it "returns nil" do
+          expect(empty_board.check_rows).to eq(nil)
+        end
+      end
+
+      context "when rows have markers" do
+        subject(:board_with_markers) { described_class.new }
+        let(:actual_board) {
+          board = Array.new(9)
+          board[0] = 'X'
+          board[3] = 'O'
+          board
+        }
+
+        before do
+          allow(Array).to receive(:new).and_return(actual_board)
+        end
+
+        it "returns nil" do
+          expect(board_with_markers.check_rows).to eq(nil)
+        end
+      end
+    end
+
+    context "when a row indicates a winner" do
+      subject(:winning_board) { described_class.new }
+      let(:actual_board) {
+        board = Array.new(9)
+        board[0..2] = ['O', 'O', 'O']
+        board
+      }
+
+      before do
+        allow(Array).to receive(:new).and_return(actual_board)
+      end
+
+      it "returns the marker of the winner" do
+        expect(winning_board.check_rows).to eq('O')
+      end
+    end
+  end
+
+
+  describe '#check_columns' do
+    context "when no columns indicate a winner" do
+      context "when no columns have any markers" do
+        subject(:empty_board) { described_class.new }
+
+        it "returns nil" do
+          expect(empty_board.check_columns).to eq(nil)
+        end
+      end
+
+      context "when columns have markers" do
+        subject(:board_with_markers) { described_class.new }
+        let(:actual_board) {
+          board = Array.new(9)
+          board[6] = 'X'
+          board[2] = 'O'
+          board
+        }
+
+        before do
+          allow(Array).to receive(:new).and_return(actual_board)
+        end
+
+        it "returns nil" do
+          expect(board_with_markers.check_columns).to eq(nil)
+        end
+      end
+    end
+
+    context "when a column indicates a winner" do
+      subject(:winning_board) { described_class.new }
+      let(:actual_board) {
+        board = Array.new(9)
+        board[0] = 'O'
+        board[3] = 'O'
+        board[6] = 'O'
+        board
+      }
+
+      before do
+        allow(Array).to receive(:new).and_return(actual_board)
+      end
+      
+      it "returns the marker of the winner" do
+        expect(winning_board.check_columns).to eq('O')
+      end
+    end
+  end
+
+
+  describe '#check_diagonals' do
+    context "when there is a winner" do
+      it "returns the marker of the winner" do
+
+      end
+    end
+
+    context "when there is no winner" do
+      subject(:no_winner_board) { described_class.new }
+      
+      context "when the board is empty" do
+        it "returns nil" do
+          expect(no_winner_board.check_diagonals).to eq(nil)
+        end
+      end
+
+      context "when the board is not empty" do
+        let(:actual_board) {
+          board = Array.new(9)
+          board[0] = 'O'
+          board[4] = 'X'
+          board
+        }
+
+        before do
+          allow(Array).to receive(:new).and_return(actual_board)
+        end
+
+        it "returns nil" do
+          expect(no_winner_board.check_diagonals).to eq(nil)
+        end
+      end
+    end
+  end
+
+
+  # Reset outputs correctly
+  after :all do
+    $stdout = @stdout
+    $stderr = @stderr
+    @stdout = nil
+    @stderr = nil
+  end
+
+end
